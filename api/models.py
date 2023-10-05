b22a4305d35e114ece691ffbaca47cb2559f7571
Nemes Tamás <nemes.tamas@mechwart.local>
2023-10-05 18:35:25 +0200

    toppings added

--- api/models.py
+++ api/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Topping(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=2000)
+
+    toppings = models.ManyToManyField(Topping)
 
     img = models.ImageField(upload_to="static/images/pizzas/", blank=True, null=True)
 
